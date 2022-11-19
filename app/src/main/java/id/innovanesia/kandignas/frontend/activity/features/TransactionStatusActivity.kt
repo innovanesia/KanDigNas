64dbf20c40d7421471b527e4279b7e31df58c88a
adryansyah29 <adryansyah707@gmail.com>
2022-11-20 00:38:33 +0700

    update new transaction update
    show sender name and sender nik/nisn

--- app/src/main/java/id/innovanesia/kandignas/frontend/activity/features/TransactionStatusActivity.kt
+++ app/src/main/java/id/innovanesia/kandignas/frontend/activity/features/TransactionStatusActivity.kt
@@ -1,35 +1,46 @@
 package id.innovanesia.kandignas.frontend.activity.features
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import id.innovanesia.kandignas.R
+import id.innovanesia.kandignas.backend.api.InitAPI
 import id.innovanesia.kandignas.backend.models.Users
+import id.innovanesia.kandignas.backend.response.AccountResponse
 import id.innovanesia.kandignas.databinding.ActivityTransactionStatusBinding
+import id.innovanesia.kandignas.frontend.activity.AuthActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.text.DecimalFormat
 import java.text.NumberFormat
 
-class TransactionStatusActivity : AppCompatActivity()
-{
+class TransactionStatusActivity : AppCompatActivity() {
     private lateinit var binds: ActivityTransactionStatusBinding
+    private lateinit var sharedPreference: SharedPreferences
+    private val keyToken = "key.token"
 
-    companion object
-    {
+    companion object {
         const val STATUS = "STATUS"
         const val TARGET = "TARGET"
         const val AMOUNT = "AMOUNT"
     }
 
     @SuppressLint("SetTextI18n")
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binds = ActivityTransactionStatusBinding.inflate(layoutInflater)
         setContentView(binds.root)
+        sharedPreference = getSharedPreferences("KanDigNas", Context.MODE_PRIVATE)
+        val token = sharedPreference.getString(keyToken, null)!!
+
 
         val status = intent.getStringExtra(STATUS)!!
         val user: Users = if (Build.VERSION.SDK_INT >= 33)
@@ -40,39 +51,31 @@ class TransactionStatusActivity : AppCompatActivity()
 
         binds.apply {
             nameContent.text = user.fullname
+            getSenderData(token)
             val format: NumberFormat = DecimalFormat("#,###")
             amountContent.text = "Rp " + format.format(amount)
-            if (status == "success")
-            {
+            if (status == "success") {
                 Glide.with(this@TransactionStatusActivity)
                     .load(R.drawable.success_alert)
                     .into(successIllustration)
                 transactionStatus.text = "Transaksi Sukses!"
-                if (user.nik != "")
-                {
+                if (user.nik != "") {
                     idTextDesc.text = "NIK"
                     idContent.text = user.nik
-                }
-                else
-                {
+                } else {
                     idTextDesc.text = "NISN"
                     idContent.text = user.nisn
                 }
-            }
-            else if (status == "failed")
-            {
+            } else if (status == "failed") {
                 Glide.with(this@TransactionStatusActivity)
                     .load(R.drawable.failed_alert)
                     .into(successIllustration)
                 transactionStatus.text = "Transaksi Gagal!"
                 seeTransactionHistoryButton.visibility = View.GONE
-                if (user.nik != "")
-                {
+                if (user.nik != "") {
                     idTextDesc.text = "NIK"
                     idContent.text = user.nik
-                }
-                else
-                {
+                } else {
                     idTextDesc.text = "NISN"
                     idContent.text = user.nisn
                 }
@@ -91,6 +94,45 @@ class TransactionStatusActivity : AppCompatActivity()
                 )
                 finish()
             }
+        }
+    }
+
+    private fun getSenderData(token: String) {
+        binds.apply {
+            InitAPI.api.getAccount("Bearer $token")
+                .enqueue(object : Callback<AccountResponse> {
+                    override fun onResponse(
+                        call: Call<AccountResponse>,
+                        response: Response<AccountResponse>
+                    ) {
+                        nameContent2.text = response.body()!!.user.fullname
+                        if (response.body()!!.user.nik != "") {
+                            idTextDesc2.text = "NIK"
+                            idContent2.text = response.body()!!.user.nik
+                        } else {
+                            idTextDesc2.text = "NISN"
+                            idContent2.text = response.body()!!.user.nisn
+                        }
+                    }
+
+
+                    override fun onFailure(call: Call<AccountResponse>, t: Throwable) {
+                        val delete: SharedPreferences.Editor = sharedPreference.edit()
+                        delete.clear().apply()
+                        Toast.makeText(
+                            this@TransactionStatusActivity,
+                            "Gagal masuk! Mohon periksa koneksi.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        startActivity(
+                            Intent(
+                                this@TransactionStatusActivity,
+                                AuthActivity::class.java
+                            )
+                        )
+                        finish()
+                    }
+                })
         }
     }
 }
